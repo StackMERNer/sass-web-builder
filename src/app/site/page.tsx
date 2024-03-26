b388f653102bf37c910f4b0aefb86b6db183f28a
Md Ashraful Islam <ms785314@gmail.com>
2024-03-26 18:32:35 +0600

    add dark and light mode toggler

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -10,6 +10,16 @@ export default function Home() {
           Plura
         </h1>
       </div>
+      <div className="flex justify-center items-center relative ">
+        <Image
+          src={"/assets/preview.png"}
+          alt="banner image"
+          height={1200}
+          width={1200}
+          className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
+        />
+        <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+      </div>
     </main>
   );
 }
